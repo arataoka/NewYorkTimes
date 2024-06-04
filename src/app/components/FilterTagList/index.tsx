332041587277b6a4ec8e8ec3bae1c08e8dd21488
arataoka <arata.okayasu@gmail.com>
2024-06-04 22:55:59 +0900

    feat: FilterTagListリファクタ
    - フィルターのラベルのつけ外し実装

--- src/app/components/FilterTagList/index.tsx
+++ src/app/components/FilterTagList/index.tsx
@@ -1,29 +1,6 @@
 import React from 'react';
+import { TAG_LIST } from '@/app/components/FilterTagList/constant';
 import { TagButton } from '@/components/TagButton';
-
-const TAG_LIST = [
-  'sports',
-  'news',
-  'technology',
-  'health',
-  'finance',
-  'education',
-  'travel',
-  'entertainment',
-  'politics',
-  'culture',
-  'science',
-  'environment',
-  'business',
-  'art',
-  'lifestyle',
-  'food',
-  'game',
-  'technology_trends',
-  'automobile',
-  'fashion',
-  'movie',
-] as const;
 
 interface FilterTagListProps {
   filterQuery: string;
@@ -33,12 +10,15 @@ const FilterTagList: React.FC<FilterTagListProps> = ({
   filterQuery,
   setFilterQuery,
 }) => {
+  const onClick = (label: string) => () =>
+    setFilterQuery(label === filterQuery ? '' : label);
+
   return TAG_LIST.map((label) => (
     <TagButton
       key={label}
       label={label}
       selected={label === filterQuery}
-      onClick={() => setFilterQuery(label)}
+      onClick={onClick(label)}
     />
   ));
 };
