c8a038708d2704055c659a663f9c450f4c91845d
arataoka <arata.okayasu@gmail.com>
2024-06-05 22:04:34 +0900

    feat: OGP設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { Metadata } from 'next';
 import StoreProvider from '@/app/StoreProvider';
+import { DUMMY_IMAGE_URL } from '@/constants';
 import { theme } from '@/theme';
 
 export const metadata: Metadata = {
   title: 'The New York Times API Demo',
   description: 'This is the demo page of New York Times API',
+  openGraph: {
+    title: 'The New York Times API Demo',
+    description: 'This is the demo page of New York Times API',
+    images: [
+      {
+        url: DUMMY_IMAGE_URL,
+        width: 800,
+        height: 600,
+        alt: 'NEW YORK TIMES API DEMO',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const DUMMY_IMAGE_URL =
+  'https://images.unsplash.com/photo-1623039405147-547794f92e9e?q=80&w=2389&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
